cb56966c523bf74e8acaf11b0a304c1e15979723
transistor <trans@jabberwocky.ca>
2022-12-20 18:22:13 -0800

    Fixed issue caused by optimizations in strlen

--- software/src/libc/common/string/strlen.c
+++ software/src/libc/common/string/strlen.c
@@ -4,10 +4,11 @@
 
 int strlen(const char *str)
 {
-	register int i = 0;
+	volatile register int i = 0;
 
-	for (; str[i] != '\0'; i++)
-		/* intentionally empty */;
+	for (; str[i] != '\0'; i++) {
+		/* intentionally empty */
+	}
 	return(i);
 }
 
